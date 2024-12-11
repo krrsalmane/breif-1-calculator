0313d3764e910840dc0605176319febad456f814
krrsalmane <karrounsalmane@gmail.com>
2024-12-11 12:25:01 +0100

    change class main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public Main() { /* compiled code */ }
+
+    public static void subtract() { /* compiled code */ }
+
+    public static void main(java.lang.String[] args) { /* compiled code */ }
+}
+
+
+
+
