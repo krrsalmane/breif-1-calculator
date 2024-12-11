f492838076cfa12e55bd898904906543f76a560f
krrsalmane <karrounsalmane@gmail.com>
2024-12-11 15:06:53 +0100

    addition and menu

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,58 @@
+import java.util.Scanner;
+
 public class Main {
-    public Main() { /* compiled code */ }
 
-    public static void subtract() { /* compiled code */ }
+    public static void subtract() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int number1 = scanner.nextInt();
 
-    public static void main(java.lang.String[] args) { /* compiled code */ }
+        System.out.print("Enter the second number: ");
+        int number2 = scanner.nextInt();
+        int result = number1-number2;
 
-    static void test () {
-        System.out.println("testignthe the test function");
+        System.out.println("The subtraction result is: " + result);
     }
+    public static void addition() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int number2 = scanner.nextInt();
+        int result = number1+number2;
+
+        System.out.println("The subtraction result is: " + result);
+    }
+
+    public  static void main (String [] args){
+        int choice;
+        do {
+            System.out.println("\t** Menu  **\n ");
+            System.out.println("2: Addition(+)");
+            System.out.println("1: Subtraction(-)");
+            System.out.println("enter the choice de 1 to 3");
+            Scanner scanner = new Scanner(System.in);
+            choice = scanner.nextInt();
+
+            switch(choice) {
+                case 1:
+                    subtract();
+
+                    break;
+                case 2:
+                    addition();
+                    break;
+                case 3:
+                    break;
+
+
+            }
+        }while (choice != 3);  // Loop ends when the user chooses 0
+
+
+    }
+
 }
 
 
