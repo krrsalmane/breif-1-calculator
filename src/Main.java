085620def344dc08b410256995050fd7b85a698d
krrsalmane <karrounsalmane@gmail.com>
2024-12-12 10:08:09 +0100

    factorial

--- src/Main.java
+++ src/Main.java
@@ -63,7 +63,17 @@ public class Main {
         System.out.print("Enter the first number: ");
         int number1 = scanner.nextInt();
         double result = Math.sqrt(number1);
-        System.out.println("The power result is: " + result);
+        System.out.println("The square root result is: " + result);
+    }
+    public static void factorial () {
+        System.out.println("enter a number");
+        Scanner scanner = new Scanner(System.in);
+        double n = scanner.nextDouble();
+        for (double i = n-1; i>=1; i--) {
+              n *=i;
+
+        }
+        System.out.println("factorial result is: "+n);
     }
 
     public  static void main (String [] args){
@@ -76,7 +86,8 @@ public class Main {
             System.out.println("3: multiply(*)");
             System.out.println("4: divide(/)");
             System.out.println("5: power(^)");
-            System.out.println("6: squareroot");
+            System.out.println("6: square root");
+            System.out.println("7: factorial");
 
             System.out.println("enter the choice de 1 to 8");
             Scanner scanner = new Scanner(System.in);
@@ -101,8 +112,9 @@ public class Main {
                 case 6:
                     squareroot();
                     break;
-
-
+                case 7:
+                    factorial();
+                    break;
 
                 case 8:
                     break;
